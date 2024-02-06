3fdd86add6f7f2fb564c9d251f5904b4955aad03
Avery Walker <spikypancakedragon@gmail.com>
2024-02-05 21:04:43 -0600

    Capped guesses at 5, user is told how many left after wrong guess

--- guess_number.py
+++ guess_number.py
@@ -1,11 +1,13 @@
 number = 10
+num_guesses = 5
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? "))
 
-while guess != 'q':
+while guess != 'q' and num_guesses > 0:
     if guess == number:
         print("Congratulations! You guessed the right number.")
         break
     else:
-        guess = int(input("Sorry, that's not correct. Try again? "))
+        num_guesses -= 1
+        guess = int(input(f"Sorry, that's not correct. You have {num_guesses} guesses left. Try again? "))
 print(f"The number was {number}. Goodbye!")
