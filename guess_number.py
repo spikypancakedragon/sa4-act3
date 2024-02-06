9054708beab2d01eb938e6993df2c4edafa186be
Avery Walker <spikypancakedragon@gmail.com>
2024-02-05 20:54:58 -0600

    Added ability to make multiple guesses or quit after a wrong guess

--- guess_number.py
+++ guess_number.py
@@ -1,8 +1,11 @@
 number = 10
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? "))
-if guess == number:
-    print("Congratulations! You guessed the right number.")
-else:
-    print(f"Sorry! The number was {number}.")
-    
+
+while guess != 'q':
+    if guess == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    else:
+        guess = int(input("Sorry, that's not correct. Try again? "))
+print(f"The number was {number}. Goodbye!")
